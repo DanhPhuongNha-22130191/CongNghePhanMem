37401cf14b3a4d241b90a3815d02310ac731acba
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2025-05-26 22:33:27 +0700

    Thay đổi WebConfig

--- CNPM/wineshop-backend/src/main/java/com/example/config/WebConfig.java
+++ CNPM/wineshop-backend/src/main/java/com/example/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://cong-nghe-phan-mem-c36y.vercel.app")
+                .allowedOrigins("https://cnpm-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
