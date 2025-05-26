4aa34fcd6552a9780042c82ab5ef0059e9932f9e
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2025-05-27 03:43:06 +0700

    fix code 2

--- CNPM/wineshop-backend/src/main/java/com/example/repository/OrderDetailRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.repository;
-
-import com.example.model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> { }
